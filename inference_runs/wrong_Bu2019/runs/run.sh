7363565c5b96f4b1b5803887df498aa973d40693
ThibeauWouters <thibeauwouters@gmail.com>
2024-08-08 15:03:22 +0200

    Example inference run Bu2019lm

--- inference_runs/wrong_Bu2019/runs/run.sh
+++ inference_runs/wrong_Bu2019/runs/run.sh
@@ -2,22 +2,22 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 
-export label=AT2017gfo_dL_free
+export label=AT2017gfo_dL40_tight_iota # choose prior, also used as label
 
 mpiexec -np 16 lightcurve-analysis \
     --model Bu2019lm \
-    --svd-path ./svdmodels/ \
+    --svd-path /home/urash/twouters/new_nmma_models \
     --outdir outdir \
     --label $label \
     --trigger-time 57982.5285236896 \
-    --data ../data/AT2017gfo.dat \
+    --data ../data/AT2017gfo_corrected.dat \
     --prior ../priors/$label.prior \
     --tmin 0.05 \
     --tmax 14 \
     --dt 0.1 \
     --error-budget 1 \
-    --nlive 1024 \
+    --nlive 2048 \
     --Ebv-max 0 \
-    --interpolation-type tensorflow \
     --local-only \
+    --interpolation-type tensorflow \
     --plot
